0bd8c40d1e1c208002aac00cf59704de76d34bc6
Patrick Corcoran <patrickcorcoran10@gmail.com>
2025-11-05 13:18:07 -0600

    deploy fix

--- app/components/jumbotron.tsx
+++ app/components/jumbotron.tsx
@@ -4,10 +4,10 @@ import React, { useEffect, useState } from "react";
 
 const Jumbotron = () => {
 //   const { theme } = useTheme();
-  const [mounted, setMounted] = useState();
+//   const [mounted, setMounted] = useState();
 
   useEffect(() => {
-    setMounted(true);
+    // setMounted(true);
   }, []);
   return (
 <div className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300`}>      
