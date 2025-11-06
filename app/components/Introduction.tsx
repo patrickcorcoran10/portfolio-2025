5e581bd26fabab564bf4ed4faa4cec528727db32
Patrick Corcoran <patrickcorcoran10@gmail.com>
2025-11-05 20:35:58 -0600

    header adn others added not styled

---
+++ app/components/Introduction.tsx
@@ -0,0 +1,11 @@
+const Introduction = () => {
+    return (
+        <div>
+            <p>
+                Hi, I’m a full stack web developer passionate about creating dynamic, user-focused web applications. I specialize in building responsive front-end interfaces and robust back-end systems using modern technologies like [React, Node.js, and MongoDB]. With a strong focus on clean code, performance, and user experience, I enjoy turning ideas into efficient, scalable digital solutions. Explore my projects below to see how I bring creativity and technical skill together to solve real-world problems.
+            </p>
+        </div>
+    )
+}
+
+export default Introduction
--- app/components/jumbotron.tsx
+++ app/components/jumbotron.tsx
@@ -11,11 +11,11 @@ const Jumbotron = () => {
   }, []);
   return (
 <div className={`w-full p-2 mob:p-4 rounded-lg transition-all ease-out duration-300`}>      
-    <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5">
+    <h1 className="text-8xl tablet:text-6xl laptop:text-8xl laptop:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5">
         Hello 👋
     </h1>
-    <h1 className="text-3xl">My name is Pat Corcoran</h1>
-        <p className="mt-5 opacity-40 text-xl">
+    <h1 className="text-8xl">My name is Pat Corcoran</h1>
+        <p className="mt-5 opacity-40 text-8xl">
             - a curious designer and developer based in Chicago, IL.      
         </p>
     </div>
